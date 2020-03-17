abcaa88bc6f0ed106480859167cb40c93773f0ac
Pierre Weyl <pweyl@thalos.fr>
2020-03-17 17:16:43 +0100

    Update link message to GuideSF

--- template/base.php
+++ template/base.php
@@ -31,7 +31,7 @@
 
         <p class="text-muted text-center mt-4 mb-0">Développé avec &hearts; par <a href="http://pwbzh.fr">Pierre Weyl</a>, bénévole chez les <a href="https://www.sgdf.fr/">Scouts et Guides de France</a>.</p>
         <p class="text-muted text-center mb-2">Ce site web n'est pas officiel. Il n'est pas édité par le Scoutisme Français.</p>
-        <p class="text-muted text-center">Connaissez-vous le site web <a href="https://guidesf.pwbzh.fr">GuideSF</a> ?</p>
+        <p class="text-muted text-center">Tu aimes ce site ? Tu aimeras sûrement <a href="https://guidesf.pwbzh.fr">GuideSF</a> !</p>
     </div>
 </body>
 
